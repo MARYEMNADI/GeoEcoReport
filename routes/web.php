5bbea54a7827152c25eb0b319079d64133b2f7c1
MARYEMNADI <nadimaryem10@gmail.com>
2026-09-15 11:41:46 +0100

    odifier

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\AffectationController;
 use App\Http\Controllers\AuthController;
-// use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IncidentController;
